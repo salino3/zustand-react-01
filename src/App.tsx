8f45337053e01b9f61dd0e7dbb517516cb50741d
Salino3 <flaviobcn012@gmail.com>
2025-03-09 12:08:17 +0100

    add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import "./App.scss";
+import { Container, Stack, Typography } from "@mui/material";
 import { LogoJS } from "./icons";
+import "./App.scss";
 
 function App() {
   return (
     <main className="root">
-      <h1 className="titleWeb">Javascript Quiz</h1>
-      <LogoJS />
+      <Container>
+        <Stack
+          direction={"row"}
+          gap={2}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <LogoJS />
+          <Typography className="titleWeb" variant="h2" component={"h1"}>
+            Jascript Quiz
+          </Typography>
+        </Stack>
+      </Container>
     </main>
   );
 }
